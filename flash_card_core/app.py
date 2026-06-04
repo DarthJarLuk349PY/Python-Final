eaafb7f4f38857e1ae4018cc14016a3a2d751ec3
DarthJarLuk349PY <cdev11g@gmail.com>
2026-06-04 18:24:19 +0000

    FINAL DAY

--- flash_card_core/app.py
+++ flash_card_core/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates')
+from flask_caching import Cache
 
+app = Flask(__name__, template_folder='templates')
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
+cache.init_app(app)
+
+#REDEIRCT TO NEXT FILES 
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -10,6 +15,31 @@ def output():
     return render_template('output.html')
 
 
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+
+@app.route('/')
+@cache.cached(timeout=50)
+def index():
+    return 'Cached for 50s'
+
+
+def make_key():
+   """A function which is called to derive the key for a computed value.
+      The key in this case is the concat value of all the json request
+      parameters. Other strategy could to use any hashing function.
+   :returns: unique string for which the value should be cached.
+   """
+   user_data = request.get_json()
+   return ",".join([f"{key}={value}" for key, value in user_data.items()])
+
+@app.route("/hello", methods=["POST"])
+@cache.cached(timeout=60, make_cache_key=make_key)
+def some_func():
+   ...
+
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port=5500, debug=True)
-
--- flash_card_core/templates/func.py
+++ flash_card_core/templates/func.py
@@ -38,14 +38,7 @@ def combine(events):
           document.querySelector("#flashcard").innerHTML = "Errror"
           return 
      full_study = dict(zip(get_term_list,get_definition_list))
-     show_next_card()
-
-
-def show_next_card():
-     global current_index
-     if current_index:
-          pass
-
+     
 
 def check_if_right():
 #SOMTHING ALONG THE LINES OF THIS WERE USER ENTERS IF THEY QUESTIO IS RIGHT OT NOT
@@ -54,13 +47,8 @@ def check_if_right():
 
      if not full_study:
          document.querySelector("#feeedback").innerHTML = "please click comine to start "
-         return
-     user_answer = document.querySelctor("#user_answee_input").value.strip()
-     c_term = get_term_list[current_index]
-     c_defintion = full_study[current_term]
-     if current_index >= len(get_term_list):
-         document.querySelector("#flashcar_display").innerHTML = "Wrong"
-         current_index += 1
-         show_next_card()
-     else:
-         document.quesrySelceto("#feedback").innerHTML = "Incorrect, Try Agian" 
+         return 
+        user_answer = document.querySelctor("#user_answer_input").value.strip()
+        else:
+            document.quesrySelceto("#feedback").innerHTML = "Incorrect, Try Agian" 
+
